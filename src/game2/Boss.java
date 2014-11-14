7273e592e9b4ca3faa9a8d5936954fa8c95c36fd
Laura Barreto <labarreto@vassar.edu>
2014-11-14 10:15:41 -0500

    starting on boss

--- src/game2/Boss.java
+++ src/game2/Boss.java
@@ -6,10 +6,38 @@
 
 package game2;
 
+import javalib.funworld.*;
+import javalib.colors.*;
+import javalib.worldimages.*;
+import java.util.*;
+import java.awt.Color;
+
 /**
  *
  * @author ldbruby95
  */
 public class Boss {
+   Posn pin;
+    String bossFileName;
+    WorldImage boss;
+    int width;
+    int height;
+    static int SCREENWIDTH = 700;
+    static int SCREENHEIGHT = 500;
+    
+     public Boss() {
+        this(new Posn(SCREENWIDTH/2, SCREENHEIGHT/2));
+    }   
+     public Boss(Posn pin) {
+        this.pin = pin;
+        bossFileName = "boss.png";
+        boss = new FromFileImage(pin, bossFileName);
+        this.width = boss.getWidth();
+        this.height = boss.getHeight();
+    }
+     
+     public WorldImage bossImage() {
+         return boss.getMovedTo(pin);
+     }
     
 }
