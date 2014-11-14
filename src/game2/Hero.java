5011e3bb76cb9d6b937f3857059410c1fa1ef9fa
Laura Barreto <labarreto@vassar.edu>
2014-11-13 22:47:34 -0500

    game stuff

--- src/game2/Hero.java
+++ src/game2/Hero.java
@@ -3,13 +3,70 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package game2;
+
+import javalib.funworld.*;
+import javalib.colors.*;
+import javalib.worldimages.*;
+import java.util.*;
+import java.awt.Color;
 
 /**
  *
  * @author ldbruby95
  */
 public class Hero {
+
+    Posn pin;
+    String heroFileName;
+    WorldImage link;
+    int width;
+    int height;
+
+    public Hero() {
+        this.pin = new Posn(Utility.randInt(0, width), Utility.randInt(0, height));
+        heroFileName = "link.png";
+        link = new FromFileImage(pin, heroFileName);
+        this.width = link.getWidth();
+        this.height = link.getHeight();
+    }
+     public Hero(Posn pin) {
+        this.pin = pin;
+        heroFileName = "link.png";
+        link = new FromFileImage(pin, heroFileName);
+        this.width = link.getWidth();
+        this.height = link.getHeight();
+    }
+
+    public WorldImage linkImage() {
+        return link.getMovedTo(pin);
+    }
     
+    
+        public Hero moveLink(String ke) {
+        int outBoundsRight = 700;
+        int outBoundsLeft = 0;
+        int outBoundsUp = 0;
+        int outBoundsDown = 500;
+        
+        Hero link = new Hero(new Posn(Utility.randInt(0, width),Utility.randInt(0,height)));
+
+        if (ke.equals("right") && ((this.pin.x + 10) <= outBoundsRight)) {
+            this.pin = new Posn(this.pin.x + 10, this.pin.y);
+            return new Hero(this.pin);
+        } else if (ke.equals("left") && ((this.pin.x - 10) >= outBoundsLeft)) {
+            this.pin = new Posn(this.pin.x - 10, this.pin.y);
+            return new Hero(this.pin);       
+        } else if (ke.equals("up") && ((this.pin.y - 10) >= outBoundsUp)) {
+            this.pin = new Posn(this.pin.x, this.pin.y - 10);
+            return new Hero(this.pin);
+        } else if (ke.equals("down") && ((this.pin.y + 10) <= outBoundsDown)) {
+            this.pin = new Posn(this.pin.x, this.pin.y + 10);
+            return new Hero(this.pin);
+        }
+        return this;
+    }
+
 }
+
+
--- src/game2/Utility.java
+++ src/game2/Utility.java
@@ -14,11 +14,11 @@ import java.util.Random;
 public class Utility {
 
     public static boolean coinToss() {
-        Random randomInts = new Random();
-        int scene = randomInts.nextInt();
-        Math.abs(scene);
-        int remainder = scene % 2;
-        return (remainder == 1 || remainder == 3);
+        if (randInt(0,1) == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static int randInt(int min, int max) {
@@ -28,12 +28,7 @@ public class Utility {
         return randomNum;
     }
     
-    public static int randInt2(int min, int max) {
-        Random rand = new Random();
-        int randomNum = rand.nextInt((max-min) +1) + min;
-        return randomNum;
-    }
-    
+
     
     
 
